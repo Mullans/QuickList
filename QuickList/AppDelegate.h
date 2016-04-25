2e5ab7c01824ec3a34f9e545d6163d163714b438
Sean Mullan <seanmm@mchsi.com>
2016-04-24 20:56:09 -0500

    Initial Commit

---
+++ QuickList/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  QuickList
+//
+//  Created by Sean Mullan on 4/24/16.
+//  Copyright © 2016 SilentLupin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ QuickList/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  QuickList
+//
+//  Created by Sean Mullan on 4/24/16.
+//  Copyright © 2016 SilentLupin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
